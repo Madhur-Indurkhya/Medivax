ae4507d43c7cfd43ab58e432983db51153c1b991
Raj kumar <rajk76986@gmail.com>
2025-03-30 22:29:48 +0530

    Removing Gemini api key for security purpose script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOUR_API_KEY'; 
+const API_KEY = 'MY_GEMINI_API_KEY'; 
 const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
 
 const chatMessages = document.getElementById('chat-messages');
