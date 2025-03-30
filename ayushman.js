c9318b351ae34c4520c90dcaa792fafeec5f46b8
Raj kumar <rajk76986@gmail.com>
2025-03-30 22:28:11 +0530

    removing api key ayushman.js

--- ayushman.js
+++ ayushman.js
@@ -1,5 +1,5 @@
-// Replace with your MapTiler API key
-const mapTilerApiKey = 'your_api_key';
+
+const mapTilerApiKey = 'my_location_api_key';
 
 // Mock data for Ayushman Kendra centers
 const mockCenters = [
